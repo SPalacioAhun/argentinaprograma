f0887c5b82554574bfc0b69fec2d61a0408a3b5f
SPalacioAhun <spa497@gmail.com>
2023-05-21 00:05:12 -0300

    practico NO finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,9 @@
+let base = 5;
+let altura = 3;
+let area = base * altura
+console.log(area)
+
+let base1 = 12.5;
+let altura1 = 5.3;
+let area1 = base1 * altura1
+console.log(area1)
---
+++ promedio3.js
@@ -0,0 +1,9 @@
+let promedio1 = (5 + 7 + 9) / 3;
+console.log(promedio1)
+
+let promedio2 = (-2 + -3 + -8) / 3;
+console.log(promedio2)
+
+let promedio3 = (-1.2 + 5.3 + -3.3) / 3;
+console.log(promedio3)
+
